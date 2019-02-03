0d762be358ec293cfb7c690bb4398fe1398deda9
Chase Hilbert <chase@heyspacetime.com>
2019-02-03 03:11:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,8 @@ end
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    array.each_with_index do |value, index|
   end
 end
